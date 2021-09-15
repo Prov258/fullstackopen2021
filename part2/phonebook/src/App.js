68e007b77d06f794a6e88ab7462908d774f4b0db
Prov <prov.offcl@gmail.com>
2021-09-15 10:27:00 +0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: "040-1234567" }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filterNames, setFilterNames ] = useState('')
 
   const submitNumberHandler = (e) => {
     e.preventDefault();
@@ -19,9 +23,20 @@ const App = () => {
     setNewNumber('')
   }
 
+  const showPhonebook = (person) => {
+      return person.name.toLowerCase().includes(filterNames.toLowerCase())
+        ? <li key={person.name}>{person.name}: {person.number}</li>
+        : null
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with
+          <input value={filterNames} onChange={(e) => setFilterNames(e.target.value)} />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={submitNumberHandler} >
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -35,7 +50,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}: {person.number}</li>)}
+        {persons.map(showPhonebook)}
       </ul>
     </div>
   )
