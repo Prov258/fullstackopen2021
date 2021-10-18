c3191474afeae94a1836ad9380febf8f9ebf7770
Prov <prov.offcl@gmail.com>
2021-10-18 21:54:39 +0300

    4.17: bloglist expansion, step5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,17 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogsRouter.get('/', async (req, res) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1, id: 1 })
     res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
-    const blog = new Blog(req.body)
-    const result = await blog.save()
+    const body = req.body
+    const user = await User.findById(body.userId)
 
-    res.status(201).json(result)
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    console.log(user.blogs)
+    await user.save()
+
+    res.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (req, res) => {
